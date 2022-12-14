67d79dad696eed942c71a282c29767eae83afe79
Pabllot07 <pabllot07@outlook.com>
2022-12-14 15:37:03 -0300

    posts react query

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,16 +1,18 @@
 import Post from "../post/Post";
 import "./posts.scss";
 import { useQuery } from 'react-query'
+import { makeRequest } from "../../axios";
 
 
 const Posts = () => {
-
   const { isLoading, error, data } = useQuery('post', () =>
-  
+    makeRequest.get("/posts").then((res) => {
+      return res.data;
+    })
    )
 
   return <div className="posts">
-    {posts.map(post=>(
+    {data.map(post=>(
       <Post post={post} key={post.id}/>
     ))}
   </div>;
