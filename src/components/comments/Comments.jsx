1ffbc8617216278d41f925f95811da5c0996ffc7
Pabllot07 <pabllot07@outlook.com>
2022-12-17 11:02:21 -0300

    writting a comment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,12 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import { makeRequest } from "../../axios";
 import moment from 'moment'
 
 const Comments = ({postId}) => {
+  const [desc, setDesc] = useState("");
   const { currentUser } = useContext(AuthContext);
+  const queryClient = useQueryClient();
+
 
   const { isLoading, error, data } = useQuery('comments', () =>
   makeRequest.get("/comments?postId="+postId).then((res) => {
@@ -14,18 +17,37 @@ const Comments = ({postId}) => {
   })
   );
 
-  console.log(data)
+  const mutation = useMutation((newComment) => {
+    return makeRequest.post("/comments", newComment)
+  }, {
+    onSuccess: ()=>{
+      //Invalid and refetch
+      queryClient.invalidateQueries(["comments"])
+    }
+  })
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({desc, postId });
+    setDesc("");
+
+  };
 
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="write a comment" />
-        <button>Send</button>
+        <input 
+        type="text"
+        placeholder="write a comment"
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)} 
+        />
+        <button onClick={handleClick}>Send</button>
       </div>
       { isLoading ? "Loading" : data.map((comment) => (
         <div className="comment">
-          <img src={comment.profilePicture} alt="" />
+          <img src={comment.profilePic} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
