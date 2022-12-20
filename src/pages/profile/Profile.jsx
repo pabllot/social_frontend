c77d298ea6f090d7668073eb85f1efc59a22303d
Pabllot07 <pabllot07@outlook.com>
2022-12-20 16:44:02 -0300

    fetch followers

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,21 +1,18 @@
 import "./profile.scss";
-import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import PinterestIcon from "@mui/icons-material/Pinterest";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Posts from "../../components/posts/Posts";
 import { useQuery, useQueryClient, useMutation } from 'react-query'
 import { makeRequest } from "../../axios";
 import { useLocation } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import { useContext } from "react";
 
 const Profile = () => {
 
-  const userId = useLocation().pathname.split("/")[2]
+  const { currentUser } = useContext(AuthContext)
+
+  const userId = parseInt(useLocation().pathname.split("/")[2])
 
   const { isLoading, error, data } = useQuery(["user"], () =>
   makeRequest.get("/users/find/"+ userId).then((res) => {
@@ -23,62 +20,56 @@ const Profile = () => {
   })
   );
 
-  console.log(data)
+  const { isLoading: rIsLoading,  data: relationshipData } = useQuery(["relationship"], () =>
+  makeRequest.get("/relationships?followedUserId="+ userId).then((res) => {
+    return res.data;
+  })
+  );
+
+  console.log(relationshipData)
+
+  const handleFollow = () => {
+
+  }
 
   return (
     <div className="profile">
+      {isLoading ? "loading..." : <>
       <div className="images">
         <img
-          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src={data?.coverPic}
           alt=""
           className="cover"
         />
         <img
-          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          src={data?.profilePic}
           alt=""
           className="profilePic"
         />
       </div>
       <div className="profileContainer">
         <div className="uInfo">
-          <div className="left">
-            <a href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <InstagramIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <TwitterIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <LinkedInIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <PinterestIcon fontSize="large" />
-            </a>
-          </div>
+          
           <div className="center">
-            <span>Jane Doe</span>
+            <span>{data?.name}</span>
             <div className="info">
               <div className="item">
                 <PlaceIcon />
-                <span>USA</span>
+                <span>{data?.city}</span>
               </div>
               <div className="item">
                 <LanguageIcon />
-                <span>lama.dev</span>
+                <span>{data?.website}</span>
               </div>
             </div>
-            <button>follow</button>
+            {rIsLoading ? "Loading" :  userId === currentUser.id ? <button>Upgrade</button> :
+             <button onClick={handleFollow}>{relationshipData.includes(currentUser.id) ? "Following" : "Follow"}</button>}
           </div>
-          <div className="right">
-            <EmailOutlinedIcon />
-            <MoreVertIcon />
-          </div>
+        
         </div>
       <Posts/>
       </div>
+      </>}
     </div>
   );
 };
