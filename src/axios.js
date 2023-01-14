c76f2acc9df215e32ef32a604c9369170a03e36b
Pabllot07 <pabllot07@outlook.com>
2023-01-14 01:02:18 -0300

    teste

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const makeRequest = axios.create({
-    baseURL: "http://localhost:8800/api/",
+    baseURL: "https://socialmediapablot.vercel.app//api/",
     withCredentials: true
 })
