804cb6d518562837bdefa6acaf7f7e07547076c6
Pabllot07 <pabllot07@outlook.com>
2023-01-14 01:06:14 -0300

    correction axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const makeRequest = axios.create({
-    baseURL: "https://socialbackend-production.up.railway.app/api/",
+    baseURL: "http://localhost:8800/api/",
     withCredentials: true
 })
