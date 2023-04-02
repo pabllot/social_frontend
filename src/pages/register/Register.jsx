fb33c0dc36c3fc8f724962d277e380a2544c5c12
Pabllot07 <pabllot07@outlook.com>
2023-04-01 21:37:55 -0300

    url changed

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,41 +1,55 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-import { Button, ButtonForm, Card, Container, Form, H1, H1Left, Input, Left, Paragraph, Right, Span } from "./styles";
+import axios from "axios";
+import {
+  Button,
+  ButtonForm,
+  Card,
+  Container,
+  Form,
+  H1,
+  H1Left,
+  Input,
+  Left,
+  Paragraph,
+  Right,
+  Span,
+} from "./styles";
 
-const Register = () => {  
-  const navigate = useNavigate()
+const Register = () => {
+  const navigate = useNavigate();
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
     name: "",
-    profilePic: 'user.png', 
-    coverPic: 'cover.png'
+    profilePic: "user.png",
+    coverPic: "cover.png",
   });
 
   const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  }
+  };
 
   const handleClick = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      await axios.post("http://socialmediapablot.vercel.app/api/auth/register", inputs);
-      navigate('/login')
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+      navigate("/login");
     } catch (error) {
-      setError(error.response.data)
+      setError(error.response.data);
     }
-  }
+  };
 
   return (
     <Container>
       <Card>
         <Left>
-        <H1Left>Pablot Social.</H1Left>
+          <H1Left>Pablot Social.</H1Left>
           <Paragraph>
-          Join our social media to see what's going on with the people around you.
+            Join our social media to see what's going on with the people around
+            you.
           </Paragraph>
           <Span>Do you have an account?</Span>
           <Link to="/login">
@@ -45,9 +59,25 @@ const Register = () => {
         <Right>
           <H1>Register</H1>
           <Form>
-            <Input type="text"  placeholder="Username" name="username" onChange={handleChange}/>
-            <Input type="text" autocomplete="off" placeholder="Name" name="name"onChange={handleChange}/>
-            <Input type="password" placeholder="Password" name="password" onChange={handleChange}/>
+            <Input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <Input
+              type="text"
+              autocomplete="off"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
             {error && error}
             <ButtonForm onClick={handleClick}>Register</ButtonForm>
           </Form>
